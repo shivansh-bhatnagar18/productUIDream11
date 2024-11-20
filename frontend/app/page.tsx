fd37ed7bb78888b721dc76a0166013ad4eb40ce1
Abhishek Punhani <punhani.manavabhi@gmail.com>
2024-11-20 23:13:07 +0530

    Initialised main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Mainpage from "./mainPage/page";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div>
-      <Mainpage /> 
+      <Navbar />
+      <Mainpage />
     </div>
   );
 }
