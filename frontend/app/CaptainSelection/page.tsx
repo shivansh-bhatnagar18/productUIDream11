144c5ad5ccfb0bc71894289bea709439dc4838ad
Abhishek Punhani <punhani.manavabhi@gmail.com>
2024-11-30 11:40:50 +0530

    Added some changes

--- frontend/app/CaptainSelection/page.tsx
+++ frontend/app/CaptainSelection/page.tsx
@@ -25,6 +25,19 @@ function page() {
     null
   );
   const [countSelected, setCountSelected] = useState<number>(0);
+  const [initial1, setInitial1] = useState<string>('');
+  const [initial2, setInitial2] = useState<string>('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const match = new URLSearchParams(window.location.search).get(
+        'match'
+      ) as string;
+      const [team1, team2] = match.split(' vs ');
+      setInitial1(team1);
+      setInitial2(team2);
+    }
+  }, []);
 
   useEffect(() => {
     const teamData = JSON.parse(
@@ -38,7 +51,7 @@ function page() {
 
   return (
     <div className="flex flex-col items-center bg-[#0D0402] min-h-screen max-w-screen min-w-screen overflow-x-hidden">
-      <Header />
+      <Header initial1={initial1} initial2={initial2} />
       {/* team selection divs */}
       <div className="max-w-[50%] min-w-[50%] mx-auto mt-8">
         <LoadingBar count={countSelected} />
