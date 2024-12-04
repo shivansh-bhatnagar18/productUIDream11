436a0ff2bd26d5bf350cfe1e854beb8a61e94e43
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2024-12-04 20:00:16 +0530

    fixing build errors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,5 @@
 'use client';
-import Image from 'next/image';
 import Mainpage from './mainPage/page';
-import Navbar from '@/components/navbar';
-import Page from './PlayerSelection/page';
 
 export default function Home() {
   return (
